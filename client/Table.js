6ad0f743b51c7766a99c1b7ab01105c33150cd1b
Carlos Saito <carlos@saito.style>
2019-12-05 16:51:43 +0100

    Show real table in frontend

--- client/Table.js
+++ client/Table.js
@@ -1,10 +1,36 @@
 import React from "react"
+import { useFetch } from "./react-hooks"
 
-function Table ({assignment, module}) {
+function Table ({course, assignment, module}) {
+  const { loading, error, data } = useFetch(`api/table?course_id=${course}&assignment_id=${assignment}&module_id=${module}`)
+
+  if (loading) return (
+    <div>Loading</div>
+  )
+
+  if (error) return (
+    <div>error</div>
+  )
+
+
   return <table border="1">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Canvas grade</th>
+        <th>Ladok grade</th>
+      </tr>
+    </thead>
     <tbody>
-      <td>Hello</td>
-      <td>World</td>
+      {
+        data.map(row => (
+          <tr>
+            <td>{row.name}</td>
+            <td>{row.canvasGrade}</td>
+            <td>{row.ladokGrade}</td>
+          </tr>
+        ))
+      }
     </tbody>
   </table>
 }
