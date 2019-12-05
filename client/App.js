7e3477c2a187823c25e8db4207ab3f410e282e60
Carlos Saito <carlos@saito.style>
2019-12-05 15:06:48 +0100

    React useFetch 2

--- client/App.js
+++ client/App.js
@@ -1,7 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function App ({ courseId }) {
+  async function fetchData () {
+    const result = await window.fetch(`api/course-info?course_id=${courseId}`)
+      .then(r => r.json())
+    console.log(result)
+  }
+
   const [assignments, setAssignments] = useState([1])
+
+  useEffect(() => {
+    fetchData()
+  })
 
   return (
     <div>
