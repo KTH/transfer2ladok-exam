c4f9fa22d3502a43dc6294d297f9ca62a4b80a04
Carlos Saito <carlos@saito.style>
2019-12-05 14:27:49 +0100

    Format

--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-const App = () => <div>Hello from  with hot loader</div>
+const App = () => <div>Hello from with hot loader</div>
 
 render(<App />, document.getElementById('root'))
