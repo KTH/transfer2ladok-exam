34966c1f127c5bd4763f076a0f0136cd88eec0d8
Carlos Saito <carlos@saito.style>
2019-12-06 09:15:08 +0100

    Babel/polyfill > core-js

--- babel.config.js
+++ babel.config.js
@@ -2,13 +2,8 @@ const presets = [
   [
     '@babel/env',
     {
-      targets: {
-        edge: '17',
-        firefox: '60',
-        chrome: '67',
-        safari: '11.1'
-      },
-      useBuiltIns: 'usage'
+      useBuiltIns: 'usage',
+      corejs: '3'
     }
   ],
   ['@babel/react']
