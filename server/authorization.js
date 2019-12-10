afc80b8037cb6b5aa8129d79af64d4a8b47bafc7
Carlos Saito <carlos@saito.style>
2019-12-10 13:45:20 +0100

    Format code

--- server/authorization.js
+++ server/authorization.js
@@ -11,7 +11,7 @@ async function authorize (req, res, next) {
   if (!accessData) {
     throw new Error('No access data found')
   }
-  
+
   // TODO: this should be !== to work!
   if (accessData.realUserId && accessData.userId === accessData.realUserId) {
     throw new ClientError(
