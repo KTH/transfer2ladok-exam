5b06999fe193df7bea323612c0b5816653001c09
Carlos Saito <carlos@saito.style>
2019-12-16 08:54:06 +0100

    Format code

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const expressHandlebars = require("express-handlebars");
-const Router = require("express-promise-router");
-const log = require("skog");
-const path = require("path");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const system = require("./system");
-const { oauth1, oauth2 } = require("./oauth")("/export3");
-const authorization = require("./authorization");
+const express = require('express')
+const expressHandlebars = require('express-handlebars')
+const Router = require('express-promise-router')
+const log = require('skog')
+const path = require('path')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const system = require('./system')
+const { oauth1, oauth2 } = require('./oauth')('/export3')
+const authorization = require('./authorization')
 const {
   rootPage,
   startPage,
@@ -16,75 +16,75 @@ const {
   submitForm,
   listCourseData,
   listGradesData
-} = require("./export-to-ladok");
-const cuid = require("cuid");
+} = require('./export-to-ladok')
+const cuid = require('cuid')
 
-const server = express();
-server.set("views", __dirname + "/views");
-server.engine("handlebars", expressHandlebars());
-server.set("view engine", "handlebars");
+const server = express()
+server.set('views', __dirname + '/views')
+server.engine('handlebars', expressHandlebars())
+server.set('view engine', 'handlebars')
 
-server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({ extended: true }));
-server.use(cookieParser(process.env.COOKIE_SIGNATURE_SECRET));
+server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(cookieParser(process.env.COOKIE_SIGNATURE_SECRET))
 
 server.use((req, res, next) => {
-  log.child({ req_id: cuid() }, next);
-});
+  log.child({ req_id: cuid() }, next)
+})
 
-const PROXY_PATH = process.env.PROXY_PATH || "";
+const PROXY_PATH = process.env.PROXY_PATH || ''
 
 // Define the router as map between routes and a set of middlewares & handlers
-const apiRouter = Router();
-const router = Router();
+const apiRouter = Router()
+const router = Router()
 
-if (process.env.NODE_ENV === "development") {
-  const webpack = require("webpack");
-  const webpackDevMiddleware = require("webpack-dev-middleware");
-  const config = require("../webpack.config.js");
-  const compiler = webpack(config);
+if (process.env.NODE_ENV === 'development') {
+  const webpack = require('webpack')
+  const webpackDevMiddleware = require('webpack-dev-middleware')
+  const config = require('../webpack.config.js')
+  const compiler = webpack(config)
 
   server.use(
     webpackDevMiddleware(compiler, {
       publicPath: `${process.env.PROXY_PATH}/dist`
     })
-  );
-  router.get("/test", authorization.setAdminCookie, showTestForm);
-  router.post("/test", submitForm);
+  )
+  router.get('/test', authorization.setAdminCookie, showTestForm)
+  router.post('/test', submitForm)
 } else {
-  router.use("/dist", express.static(path.resolve(process.cwd(), "dist")));
+  router.use('/dist', express.static(path.resolve(process.cwd(), 'dist')))
 }
 
-router.get("/", rootPage);
-router.post("/export", startPage);
-router.post("/export2", oauth1);
-router.get("/export3", oauth2, authorization.authorize, showForm);
-router.post("/export3", submitForm);
+router.get('/', rootPage)
+router.post('/export', startPage)
+router.post('/export2', oauth1)
+router.get('/export3', oauth2, authorization.authorize, showForm)
+router.post('/export3', submitForm)
 
-router.get("/_monitor", system.monitor);
-router.get("/_monitor_all", system.monitor);
-router.get("/_about", system.about);
-router.use("/api", apiRouter);
+router.get('/_monitor', system.monitor)
+router.get('/_monitor_all', system.monitor)
+router.get('/_about', system.about)
+router.use('/api', apiRouter)
 
-apiRouter.use(authorization.authorize);
-apiRouter.get("/course-info", listCourseData);
-apiRouter.get("/table", listGradesData);
+apiRouter.use(authorization.authorize)
+apiRouter.get('/course-info', listCourseData)
+apiRouter.get('/table', listGradesData)
 
-server.use(PROXY_PATH, router);
-server.use(function catchKnownError(err, req, res, next) {
-  if (err.name === "ClientError") {
-    log.warn({ req, res, err });
-    res.render("error", {
+server.use(PROXY_PATH, router)
+server.use(function catchKnownError (err, req, res, next) {
+  if (err.name === 'ClientError') {
+    log.warn({ req, res, err })
+    res.render('error', {
       prefix_path: process.env.PROXY_PATH,
       message: err.message,
       layout: false
-    });
+    })
   } else {
-    next(err);
+    next(err)
   }
-});
-server.use(function catchAll(err, req, res, next) {
-  log.error({ req, res, err });
-  res.send("A fatal error occurred! :(");
-});
-module.exports = server;
+})
+server.use(function catchAll (err, req, res, next) {
+  log.error({ req, res, err })
+  res.send('A fatal error occurred! :(')
+})
+module.exports = server
