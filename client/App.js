4407c40471e89ff06ac727158a989e5d0df25d25
Peter Delmeby <peter.delmeby@gmail.com>
2019-12-23 13:48:15 +0100

    Fixed styling on startpage and step 1.

--- client/App.js
+++ client/App.js
@@ -24,26 +24,30 @@ function App ({ courseId }) {
 
   let disabled = false
   let title = ''
+  let buttonClassNames = 'btn btn-success'
   if (!selectedAssignment) {
     disabled = true
     title = 'Choose an assignment in Canvas first'
+    buttonClassNames = buttonClassNames.concat(' ', 'disabled')
   } else if (!selectedModule) {
     disabled = true
     title = 'Choose a module in Ladok first'
+    buttonClassNames = buttonClassNames.concat(' ', 'disabled')
   } else if (!examinationDate) {
     disabled = true
     title = 'Choose an examination date first'
+    buttonClassNames = buttonClassNames.concat(' ', 'disabled')
   }
 
   const nextButton = (
-    <input
-      type='button'
-      className='btn btn-info'
+    <button
+      className={buttonClassNames}
       disabled={disabled}
       title={title}
       onClick={event => setCurrentPage(2)}
-      value='Show students and results →'
-    ></input>
+    >
+      Show students and results →
+    </button>
   )
 
   const content0 = (
@@ -60,42 +64,42 @@ function App ({ courseId }) {
       </h1>
       <h2>Canvas assignment:</h2>
       <p>Note that only letter grades will be sent to Ladok</p>
-      <select
-        className={
-          selectedAssignment ? 'form-control ' : 'form-control required_input'
-        }
-        value={selectedAssignment || ''}
-        name='canvas_assignment'
-        onChange={event => setAssignment(event.target.value)}
-      >
-        <option value='' disabled hidden>
-          Choose assignment
-        </option>
-        {allAssignments.map(assignment => (
-          <option key={assignment.id} value={assignment.id}>
-            {assignment.name}
+      <div className='select-wrapper'>
+        <select
+          className='custom-select'
+          value={selectedAssignment || ''}
+          name='canvas_assignment'
+          onChange={event => setAssignment(event.target.value)}
+        >
+          <option value='' disabled hidden>
+            Choose assignment
           </option>
-        ))}
-      </select>
+          {allAssignments.map(assignment => (
+            <option key={assignment.id} value={assignment.id}>
+              {assignment.name}
+            </option>
+          ))}
+        </select>
+      </div>
       <h2>Ladok Module</h2>
       <p>To which Ladok module do you want the results to be exported?</p>
-      <select
-        className={
-          selectedModule ? 'form-control ' : 'form-control required_input'
-        }
-        name='ladok_module'
-        value={selectedModule || ''}
-        onChange={event => setModule(event.target.value)}
-      >
-        <option value='' disabled hidden>
-          Choose Ladok module
-        </option>
-        {allModules.map(ladokModule => (
-          <option key={ladokModule.id} value={ladokModule.id}>
-            {ladokModule.name} - {ladokModule.title}
+      <div className='select-wrapper'>
+        <select
+          className='custom-select'
+          name='ladok_module'
+          value={selectedModule || ''}
+          onChange={event => setModule(event.target.value)}
+        >
+          <option value='' disabled hidden>
+            Choose Ladok module
           </option>
-        ))}
-      </select>
+          {allModules.map(ladokModule => (
+            <option key={ladokModule.id} value={ladokModule.id}>
+              {ladokModule.name} - {ladokModule.title}
+            </option>
+          ))}
+        </select>
+      </div>
       <h2>Examination Date</h2>
       <p>
         Required field. When exporting to Ladok, all students will receive the
@@ -104,9 +108,7 @@ function App ({ courseId }) {
       </p>
       <input
         name='examination_date '
-        className={
-          examinationDate ? 'form-control ' : 'form-control required_input'
-        }
+        className='form-control'
         type='date'
         value={examinationDate}
         onChange={event => setExaminationDate(event.target.value)}
@@ -114,12 +116,12 @@ function App ({ courseId }) {
       />
       <input type='hidden' name='course_id' value={courseId} />
       <div className='form-footer'>
-        <input
-          type='button'
-          className='btn btn-warn form-footer-btn'
+        <button
+          className='btn btn-secondary'
           onClick={event => setCurrentPage(0)}
-          value='Cancel'
-        ></input>
+        >
+          Cancel
+        </button>
         {nextButton}
       </div>
     </div>
