58241ad9280a6340826542e6ce0856b2f25c9f5d
Peter Delmeby <peter.delmeby@gmail.com>
2019-12-27 14:41:45 +0100

    Moved around content somewhat.

--- client/App.js
+++ client/App.js
@@ -50,91 +50,6 @@ function App ({ courseId }) {
     </button>
   )
 
-  const content0 = (
-    <h1 className='alert alert-success'>
-      Export cancelled. You can safely leave this page and go wherever you want
-      to.
-    </h1>
-  )
-
-  const content1 = (
-    <div className='form-group'>
-      <h1>Select assignment and date (Step 1 of 2)</h1>
-      <h2>Canvas assignment</h2>
-      <p>Note that only letter grades will be sent to Ladok</p>
-      <div className='select-wrapper'>
-        <select
-          className='custom-select'
-          value={(selectedAssignment && selectedAssignment.id) || ''}
-          name='canvas_assignment'
-          onChange={event =>
-            setAssignment({
-              id: event.target.value,
-              name: event.target.selectedOptions[0].text
-            })
-          }
-        >
-          <option value='' disabled hidden>
-            Select assignment
-          </option>
-          {allAssignments.map(assignment => (
-            <option key={assignment.id} value={assignment.id}>
-              {assignment.name}
-            </option>
-          ))}
-        </select>
-      </div>
-      <h2>Ladok Module</h2>
-      <p>To which Ladok module do you want the results to be exported?</p>
-      <div className='select-wrapper'>
-        <select
-          className='custom-select'
-          name='ladok_module'
-          value={(selectedModule && selectedModule.id) || ''}
-          onChange={event =>
-            setModule({
-              id: event.target.value,
-              name: event.target.selectedOptions[0].text
-            })
-          }
-        >
-          <option value='' disabled hidden>
-            Select Ladok module
-          </option>
-          {allModules.map(ladokModule => (
-            <option key={ladokModule.id} value={ladokModule.id}>
-              {ladokModule.name} - {ladokModule.title}
-            </option>
-          ))}
-        </select>
-      </div>
-      <h2>Examination Date</h2>
-      <p>
-        Required field. When exporting to Ladok, all students will receive the
-        same Examination Date. If you need to set a different date individually,
-        please change it in Ladok after exporting.
-      </p>
-      <input
-        name='examination_date '
-        className='form-control'
-        type='date'
-        value={examinationDate}
-        onChange={event => setExaminationDate(event.target.value)}
-        required
-      />
-      <input type='hidden' name='course_id' value={courseId} />
-      <div className='button-section'>
-        <button
-          className='btn btn-secondary grid-col-2'
-          onClick={event => setCurrentPage(0)}
-        >
-          Cancel
-        </button>
-        {nextButton}
-      </div>
-    </div>
-  )
-
   const tableFooter = (
     <div className='button-section'>
       <button
@@ -156,39 +71,126 @@ function App ({ courseId }) {
       </button>
     </div>
   )
-  const content2 = (
-    <div className='form-group'>
-      <input type='hidden' name='course_id' value={courseId} />
-      <h2>Export students with results (Step 2 of 2)</h2>
-      <div className='alert alert-info' aria-live='polite' role='alert'>
-        <p>
-          Note that the results of students are based on data fetched from
-          Canvas Gradebook during launch of this application. If you have
-          entered a result very recently and it is missing, you might have to
-          relaunch the application.
-        </p>
-      </div>
-      {showTable && (
-        <div>
-          <Table
-            course={courseId}
-            assignment={selectedAssignment}
-            module={selectedModule}
-            date={examinationDate}
-          />
-          {tableFooter}
-        </div>
-      )}
-    </div>
-  )
 
   let content = ''
   if (currentPage === 0) {
-    content = content0
+    content = (
+      <h1 className='alert alert-success'>
+        Export cancelled. You can safely leave this page and go wherever you
+        want to.
+      </h1>
+    )
   } else if (currentPage === 1) {
-    content = content1
+    content = (
+      <div className='form-group'>
+        <h1>Select assignment and date (Step 1 of 2)</h1>
+        <h2>Canvas assignment</h2>
+        <p>Note that only letter grades will be sent to Ladok</p>
+        <div className='select-wrapper'>
+          <select
+            className='custom-select'
+            value={(selectedAssignment && selectedAssignment.id) || ''}
+            name='canvas_assignment'
+            onChange={event =>
+              setAssignment({
+                id: event.target.value,
+                name: event.target.selectedOptions[0].text
+              })
+            }
+          >
+            <option value='' disabled hidden>
+              Select assignment
+            </option>
+            {allAssignments.map(assignment => (
+              <option key={assignment.id} value={assignment.id}>
+                {assignment.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <h2>Ladok Module</h2>
+        <p>To which Ladok module do you want the results to be exported?</p>
+        <div className='select-wrapper'>
+          <select
+            className='custom-select'
+            name='ladok_module'
+            value={(selectedModule && selectedModule.id) || ''}
+            onChange={event =>
+              setModule({
+                id: event.target.value,
+                name: event.target.selectedOptions[0].text
+              })
+            }
+          >
+            <option value='' disabled hidden>
+              Select Ladok module
+            </option>
+            {allModules.map(ladokModule => (
+              <option key={ladokModule.id} value={ladokModule.id}>
+                {ladokModule.name} - {ladokModule.title}
+              </option>
+            ))}
+          </select>
+        </div>
+        <h2>Examination Date</h2>
+        <p>
+          Required field. When exporting to Ladok, all students will receive the
+          same Examination Date. If you need to set a different date
+          individually, please change it in Ladok after exporting.
+        </p>
+        <input
+          name='examination_date '
+          className='form-control'
+          type='date'
+          value={examinationDate}
+          onChange={event => setExaminationDate(event.target.value)}
+          required
+        />
+        <input type='hidden' name='course_id' value={courseId} />
+        <div className='button-section'>
+          <button
+            className='btn btn-secondary grid-col-2'
+            onClick={event => setCurrentPage(0)}
+          >
+            Cancel
+          </button>
+          {nextButton}
+        </div>
+      </div>
+    )
   } else if (currentPage === 2) {
-    content = content2
+    content = (
+      <div className='form-group'>
+        <input type='hidden' name='course_id' value={courseId} />
+        <input
+          type='hidden'
+          name='canvas_assignment'
+          value={selectedAssignment.id}
+        />
+        <input type='hidden' name='ladok_module' value={selectedModule.id} />
+        <input type='hidden' name='examination_date' value={examinationDate} />
+        <h2>Export students with results (Step 2 of 2)</h2>
+        <div className='alert alert-info' aria-live='polite' role='alert'>
+          <p>
+            Note that the results of students are based on data fetched from
+            Canvas Gradebook during launch of this application. If you have
+            entered a result very recently and it is missing, you might have to
+            relaunch the application.
+          </p>
+        </div>
+        {showTable && (
+          <div>
+            <Table
+              course={courseId}
+              assignment={selectedAssignment}
+              module={selectedModule}
+              date={examinationDate}
+            />
+            {tableFooter}
+          </div>
+        )}
+      </div>
+    )
   }
 
   return <div>{content}</div>
