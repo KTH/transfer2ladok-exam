d19b03af462899e1ee579e71ddbd67b49a85dbb4
Emil <emilsten@kth.se>
2020-01-02 13:04:11 +0100

    removed some html tags

--- client/WizardConfirm.js
+++ client/WizardConfirm.js
@@ -1,55 +1,55 @@
-import React from 'react'
-import Table from './Table'
-import { ButtonModal } from '@kth/kth-style-react-components'
+import React from "react";
+import Table from "./Table";
+import { ButtonModal } from "@kth/kth-style-react-components";
 
-function WizardConfirm ({
+function WizardConfirm({
   setCurrentPage,
   selectedAssignment,
   selectedModule,
   examinationDate,
   courseId
 }) {
-  const showTable = selectedAssignment && selectedModule
+  const showTable = selectedAssignment && selectedModule;
 
   const tableFooter = (
-    <div className='button-section'>
+    <div className="button-section">
       <button
-        type='button'
-        className='btn btn-secondary grid-col-1'
+        type="button"
+        className="btn btn-secondary grid-col-1"
         onClick={event => setCurrentPage(1)}
       >
         ← Assignments
       </button>
       <button
-        type='button'
-        className='btn btn-secondary grid-col-2'
+        type="button"
+        className="btn btn-secondary grid-col-2"
         onClick={event => setCurrentPage(0)}
       >
         Cancel
       </button>
       <ButtonModal
-        id='export'
-        type='submit'
-        btnLabel='Export to Ladok'
+        id="export"
+        type="submit"
+        btnLabel="Export to Ladok"
         handleParentConfirm={() => {
-          setCurrentPage(3)
+          setCurrentPage(3);
         }}
         modalLabels={{
-          header: 'Confirm export',
+          header: "Confirm export",
           body: `<br>Canvas assignment: <strong>${selectedAssignment.name}</strong><br>Ladok module: <strong>${selectedModule.name}</strong><br>Date: <strong>${examinationDate}</strong><br><br>Do you want to proceed?`,
-          btnCancel: 'No, go back',
-          btnConfirm: 'Yes, export'
+          btnCancel: "No, go back",
+          btnConfirm: "Yes, export"
         }}
-        className='grid-col-3'
+        className="grid-col-3"
         disabled={false}
       />
     </div>
-  )
+  );
 
   return (
-    <div className='form-group'>
+    <form method="post">
       <h2>Export students with results (Step 2 of 2)</h2>
-      <div className='alert alert-info' aria-live='polite' role='alert'>
+      <div className="alert alert-info" aria-live="polite" role="alert">
         <p>
           Note that the results of students are based on data fetched from
           Canvas Gradebook during launch of this application. If you have
@@ -68,8 +68,8 @@ function WizardConfirm ({
           {tableFooter}
         </div>
       )}
-    </div>
-  )
+    </form>
+  );
 }
 
-export default WizardConfirm
+export default WizardConfirm;
