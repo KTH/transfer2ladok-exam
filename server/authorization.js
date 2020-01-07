11ab9dfa5e829980a58fc491de3965559e668f45
Carlos Saito <carlos@saito.style>
2020-01-07 10:38:48 +0100

    Reformat

--- server/authorization.js
+++ server/authorization.js
@@ -1,51 +1,51 @@
-const log = require("skog");
-const isAllowed = require("../lib/is-allowed");
-const { ClientError } = require("../lib/errors");
+const log = require('skog')
+const isAllowed = require('../lib/is-allowed')
+const { ClientError } = require('../lib/errors')
 
-async function authorize(req, res, next) {
-  const accessData = req.accessData || req.signedCookies.access_data;
-  const courseId = req.query.course_id || req.body.course_id;
+async function authorize (req, res, next) {
+  const accessData = req.accessData || req.signedCookies.access_data
+  const courseId = req.query.course_id || req.body.course_id
 
-  req.accessData = accessData;
+  req.accessData = accessData
 
   if (!accessData) {
-    throw new Error("No access data found");
+    throw new Error('No access data found')
   }
 
   if (accessData.realUserId && accessData.userId !== accessData.realUserId) {
     throw new ClientError(
-      "not_allowed",
+      'not_allowed',
       'You are not allowed to use this app in Masquerade mode ("acting as" a different user)'
-    );
+    )
   }
 
   try {
-    const allowedInLadok = await isAllowed.isAllowedInLadok(accessData.token);
+    const allowedInLadok = await isAllowed.isAllowedInLadok(accessData.token)
     if (!allowedInLadok) {
       throw new ClientError(
-        "not_allowed",
-        "You must have permissions to write results in Ladok to use this function."
-      );
+        'not_allowed',
+        'You must have permissions to write results in Ladok to use this function.'
+      )
     }
     const allowedIncanvas = await isAllowed.isAllowedInCanvas(
       accessData.token,
       courseId
-    );
+    )
 
     if (!allowedIncanvas) {
       throw new ClientError(
-        "not_allowed",
-        "Only teachers and examiners are allowed to use this app."
-      );
+        'not_allowed',
+        'Only teachers and examiners are allowed to use this app.'
+      )
     }
   } catch (err) {
-    log.error("could not authorize user properly", err);
-    return next(err);
+    log.error('could not authorize user properly', err)
+    return next(err)
   }
 
-  next();
+  next()
 }
 
 module.exports = {
   authorize
-};
+}
