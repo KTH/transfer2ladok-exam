b5e6ca6dadf6f86d91afe69bf04d1d756c49582b
Carlos Saito <carlos@saito.style>
2020-01-14 15:41:46 +0100

    Send > Transfer

--- client/WizardForm.js
+++ client/WizardForm.js
@@ -43,13 +43,15 @@ function WizardForm ({
     <div className='form-group form-select'>
       <h1>Select assignment and date (Step 1 of 2)</h1>
       <p>
-        To be able to send grades to from Canvas to Ladok, you need to map a
+        To be able to transfer grades to from Canvas to Ladok, you need to map a
         Canvas assignment to a Ladok module. Please select both a Canvas
         assignment as source, a Ladok module as target and an examination date
-        for the grades to be sent, before you can proceed.
+        for the grades to be transfered, before you can proceed.
       </p>
       <h2>Canvas assignment</h2>
-      <p>Note that only letter grades will be sent to Ladok (A-F & P/F)</p>
+      <p>
+        Note that only letter grades will be transfered to Ladok (A-F & P/F)
+      </p>
       <div className='select-wrapper'>
         <select
           className='custom-select'
