be90c7297ddcb8df71f9332016832f3dc762990f
Carlos Saito <carlos@saito.style>
2020-04-17 14:20:07 +0200

    Support examination rooms with one section

--- scripts/some-tests/omtenta.js
+++ scripts/some-tests/omtenta.js
@@ -8,9 +8,11 @@ const ladok = require('../../lib/ladok')
 
 async function start () {
   // Assume a specific Ladok examination
+  // Aktitivitetstillfälle ID
   const examId = 'd3289b77-6780-11ea-bbe9-d6135c344c05'
 
   // From Kopps Course HF1008 TEN2
+  // "Modul ID" = Aktivitets ID
   const moduleId = '4cb831ea-73d8-11e8-b4e0-063f9afb40e3'
   try {
     const gradeableResults = await ladok.listExamGradeableResults(examId)
